0b30e7b18aae749a67a3869c9bd948d0afa6d5e1
nazonohito51 <nazonohito51@hotmail.com>
2015-11-03 17:50:03 +0900

    add layout template files

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,1 +1,19 @@
-this is test.
+@extends('layouts.default_layout')
+
+@section('addCss')
+    <link href="{{{ asset('/assets/css/sample.css') }}}" rel="stylesheet">
+@stop
+
+@include('layouts.header')
+
+@section('content')
+    <body>
+        <h1>Hello</h1>
+        <div>コンテンツ</div>
+@stop
+
+@section('addJs')
+    <script type="text/javascript" src="{{{ asset('/assets/js/sample.js') }}}"></script>
+@stop
+
+@include('layouts.footer')
