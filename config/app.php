cf45c14f7621095b1eb8fadef70f13326963c5a7
nazonohito51 <nazonohito51@hotmail.com>
2015-11-08 23:51:48 +0900

    add Html package

--- config/app.php
+++ config/app.php
@@ -145,6 +145,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        'Illuminate\Html\HtmlServiceProvider',
+
     ],
 
     /*
@@ -193,6 +195,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Form' => 'Illuminate\Html\FormFacade',
+        'Html' => 'Illuminate\Html\HtmlFacade',
 
     ],
 
